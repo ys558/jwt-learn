5ff6ea035e65946a51275c7ff6bad126a0cffade
yuyi <38847066+ys558@users.noreply.github.com>
2021-06-23 10:52:57 +0800

    authenticateToken 模块化作为中间件

--- server.js
+++ server.js
@@ -6,22 +6,33 @@ const jwt = require('jsonwebtoken')
 app.use(express.json())
 
 const posts = [
-  {username: 'Kyle', title: 'POST 1'},
-  {username: 'Jim', title: 'POST 2'},
+  {username: 'Ryan', title: 'POST 1'},
+  {username: 'Sandy', title: 'POST 2'},
 ]
 
-app.get('/posts', (req, res) => {
-  res.json(posts)
+app.get('/posts', authenticateToken, (req, res) => {   
+  res.json(posts.filter(post => post.username === req.user.name))
 })
 
 app.post('/login', (req,res) => {
   // 登录token鉴权
   const username = req.body.username
   const user = { name: username }
-
   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
   res.json({ accessToken })
 })
+
+// middleware to judge token
+function authenticateToken (req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+  if (token == null) return res.sendStatus(401)
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+    req.user = user
+    next()
+  })
+}
 
 const PORT = 3000
 app.listen(PORT, ()=> console.log(`listening on port ${PORT}`))
