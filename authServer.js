570e2734ebb4dafd45435e74c7a72990d7c61fe0
yuyi <38847066+ys558@users.noreply.github.com>
2021-06-25 09:28:52 +0800

    修复bug

--- authServer.js
+++ authServer.js
@@ -22,7 +22,7 @@ let refreshTokens = []
 app.post('/token', (req, res) => {
   const refreshToken = req.body.token
   if (refreshToken == null) return res.sendStatus(401)
-  if (refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
   jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403)
     const accessToken = generateAccessToken({ name: user.name })
